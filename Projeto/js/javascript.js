1e60fe55d875f428c2026405cf07b77a32cec2d2
andrecosta <squad21github@gmail.com>
2022-01-17 19:24:36 -0300

    Atualização

--- Projeto/js/javascript.js
+++ Projeto/js/javascript.js
@@ -1,9 +1,13 @@
 let div = document.getElementById('carr')
 
+let div2 = document.getElementById("precarr")
+
 function mostra() {
     div.hidden = false
+    div2.hidden = true
 }
 
 function esconde() {
     div.hidden = true
+    div2.hidden = false
 }
